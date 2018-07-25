a49cc133ab28357828303c733a7ffad6699f329d
Muhammad Tajjiou <shutsugan@gmail.com>
2018-07-25 14:17:08 +0100

    Restructure and clean the project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Content here!
+      </div>
+    );
+  }
+}
+
+export default App;
